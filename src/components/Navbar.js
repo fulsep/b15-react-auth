0d302a9198cadabd58a9d4acaa2c85a05c2c7517
Fahrul Septiana <fahrulseptiana@gmail.com>
2020-03-26 16:14:26 +0700

    add loading state for logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import {
 
 import history from '../utils/history'
 import styled from 'styled-components'
+import Loading from '../components/Loading'
 
 const Logout = styled('span')`
   cursor: pointer;
@@ -14,27 +15,39 @@ const Logout = styled('span')`
 export default class Navbar extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      isLoading: false
+    }
     this.onLogout = () => {
-      localStorage.removeItem('token')
-      this.props.check()
-      history.push('/login')
+      this.setState({isLoading: true},()=>{
+        setTimeout(()=>{
+          this.setState({isLoading: false}, ()=>{
+            localStorage.removeItem('token')
+            this.props.check()
+            history.push('/login')
+          })
+        },1000)
+      })
     }
   }
   render() {
     return (
-      <NavigationBar color='dark' dark expand='md'>
-        <NavbarBrand>Ticketing</NavbarBrand>
-        <NavbarToggler />
-        <Collapse isOpen={true} navbar>
-          <Nav className='ml-auto' navbar>
-            {this.props.isLogin &&(
-              <NavItem>
-                <Logout className='nav-link' onClick={this.onLogout}>Logout</Logout>
-              </NavItem>
-            )}
-          </Nav>
-        </Collapse>
-      </NavigationBar>
+      <>
+        <NavigationBar color='dark' dark expand='md'>
+          <NavbarBrand>Ticketing</NavbarBrand>
+          <NavbarToggler />
+          <Collapse isOpen={true} navbar>
+            <Nav className='ml-auto' navbar>
+              {this.props.isLogin &&(
+                <NavItem>
+                  <Logout className='nav-link' onClick={this.onLogout}>Logout</Logout>
+                </NavItem>
+              )}
+            </Nav>
+          </Collapse>
+        </NavigationBar>
+        {this.state.isLoading && (<Loading/>)}
+      </>
     )
   }
 }
